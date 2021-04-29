2232aedc7349c142223751252420fa3c7116225c
Khaled Osama <khaled.osama.elhabibi@gmail.com>
2021-04-29 18:14:54 +0200

    hereF

--- src/main-components/mainTable.js
+++ src/main-components/mainTable.js
@@ -15,24 +15,32 @@ class Row extends React.Component {
         );
     }
 }
+class MainRow extends React.Component {
+    render() {
+        return (
+            <tr className = "main-table__main-row">
+            <td className = "main-table__main-row__cell display-flex">
+                DATE
+                <span className="material-icons">
+                    unfold_more
+                </span>
+            </td>
+            <td className = "main-table__main-row__cell">PRODUCT</td>
+            <td className = "main-table__main-row__cell">SIZE (US)</td>
+            <td className = "main-table__main-row__cell">WEBSITE</td>
+            <td className = "main-table__main-row__cell">PRICE</td>
+            <td className = "main-table__main-row__cell">STATUS</td>
+        </tr>                    
+        );
+    }
+}
 
 class MainTable extends React.Component {
     render() {
         return (
+            <div className = "main-table__container">
             <table className = "main-table">
-                <tr className = "main-table__main-row">
-                    <td className = "main-table__main-row__cell display-flex">
-                        DATE
-                        <span className="material-icons">
-                            unfold_more
-                        </span>
-                    </td>
-                    <td className = "main-table__main-row__cell">PRODUCT</td>
-                    <td className = "main-table__main-row__cell">SIZE (US)</td>
-                    <td className = "main-table__main-row__cell">WEBSITE</td>
-                    <td className = "main-table__main-row__cell">PRICE</td>
-                    <td className = "main-table__main-row__cell">STATUS</td>
-                </tr>
+                <MainRow/>
                 <Row product = "Jordan 11 Retro Jubilee" size = {9.0} price = "$211" status = "SUCCESS" />
                 <Row product = "Jordan 11 Retro Jubilee" size = {9.0} price = "$211" status = "SUCCESS" />
                 <Row product = "Jordan 6 Retro Carmine (2021)" size = {10.0} price = "$252" status = "FAIL" />
@@ -59,7 +67,7 @@ class MainTable extends React.Component {
                 <Row product = "Jordan 11 Retro Jubilee" size = {9.0} price = "$211" status = "SUCCESS" />
 
             </table>
-
+            </div>
         );
     }
 }
